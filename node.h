2c35544bd1e0e7bd7391a108bd6e1246f5aed492
lspc678 <dltmdqls1945@naver.com>
2023-11-20 17:51:41 +0900

    refactor: changed Node class structure (#15)

--- node.h
+++ node.h
@@ -3,46 +3,15 @@
 #ifndef NODE_H
 #define NODE_H
 
-// 복사 생성자, 대입 연산자 사용 방지
-#define DISALLOW_COPY_AND_ASSIGN(Node) \
-    Node(const Node &);                \
-    void operator=(const Node &)
-
+// Node의 경우 Abstract Class로 정의 (pure virtual function 이용)
 class Node
 {
 public:
-    Node(int num)
-        : num_(num), height_(0), 
-        parent_(nullptr), left_(nullptr), 
-        right_(nullptr) {}
-    ~Node();
-    void SetParent(Node* parent);
-    void SetLeft(Node* left);
-    void SetRight(Node* right);
-    void SetHeight(const int height);
-    int GetNum();
-    int GetHeight();
-    Node* GetParent();
-    Node* GetLeft();
-    Node* GetRight();
-private:
-    // 복사 생성자, 대입 연산자 사용 방지
-    DISALLOW_COPY_AND_ASSIGN(Node);
-
-    // 해당 node의 key값
-    int num_;
-
-    // 해당 node의 child 중 height_의 최댓값 + 1 (leaf node일 경우 0)
-    int height_;
-
-    // 부모 노드
-    Node* parent_;
-
-    // Left Child 노드
-    Node* left_;
-
-    // Right Child 노드
-    Node* right_;
+    virtual ~Node() = 0;
+    virtual int GetNum() const = 0;
 };
+
+// Node 클래스의 소멸자 정의
+Node::~Node() {}
 
 #endif
