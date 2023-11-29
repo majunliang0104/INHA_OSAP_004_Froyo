df804380aaaaf07195b2058e980cb3ff39b3da94
majunliang0104 <majunliang@naver.com>
2023-11-29 20:00:31 +0900

    feat : Maximum 기능 구현 #30

--- set_avl.cc
+++ set_avl.cc
@@ -13,7 +13,23 @@ void SetAVL::Minimum(const int key)
 // key를 root로 하는 subtree에서 최댓값을 갖는 node의 값과 depth를 출력
 void SetAVL::Maximum(const int key)
 {
-
+    NodeAVL* node = root_;
+    // key를 root로 하는 node찾기
+    while(node != nullptr)
+    {
+        if(key == node->GetNum())
+            break;
+        else if(key < node->GetNum())
+            node = node->GetLeft();
+        else
+            node = node->GetRight();
+    }
+    // subtree에서 최댓값을 갖는 node찾기
+    while(node != nullptr && node->GetRight() != nullptr)
+    {
+        node = node->GetRight();
+    }
+    std::cout << "key : " << node->GetNum() << ", depth : " << Find(key);
 }
 
 // 해당 key를 가지고 있는 node의 depth를 return
